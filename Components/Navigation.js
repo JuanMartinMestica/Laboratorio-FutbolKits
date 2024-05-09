0ce6985b28b69f91232cbc0546684b4475b6e47a
Juan Martín Mestica <juan.mestica@est.fi.uncoma.edu.ar>
2024-05-09 13:30:06 -0300

    Se agregan iconos a la navegación

--- Components/Navigation.js
+++ Components/Navigation.js
@@ -1,42 +1,79 @@
-  import React from "react";
-  import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-  import { createStackNavigator } from "@react-navigation/stack";
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
 
-  //Pages
-  import Home from "../Pages/Home";
-  import Productos from "../Pages/Productos";
-  import Camiseta from "../Pages/Camiseta";
-  import { NavigationContainer } from "@react-navigation/native";
-  import Carga from "../Pages/Carga";
+//Pages
+import Home from "../Pages/Home";
+import Productos from "../Pages/Productos";
+import Camiseta from "../Pages/Camiseta";
+import { NavigationContainer } from "@react-navigation/native";
+import Carga from "../Pages/Carga";
 
-  const Tab = createBottomTabNavigator();
-  const Stack = createStackNavigator(); 
+import { Ionicons } from "@expo/vector-icons"; // Importa los iconos de Ionicons
 
-  function ProductosStack() {
-    return (
-      <Stack.Navigator>
-        <Stack.Screen name="Productos" component={Productos} options={{headerShown: false}} />
-        <Stack.Screen name="Camiseta" component={Camiseta} options={{headerShown: false}} />
-      </Stack.Navigator>
-    );
-  }
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
-  function MyTabs() {
-    return (
-      <Tab.Navigator screenOptions={{tabBarActiveTintColor: "#FC2F00"}}>
-        <Tab.Screen name="Home" component={Home}  options={{headerShown: false}}  />
-        <Tab.Screen name="Camisetas" component={ProductosStack} options={{headerShown: false}} />
-        <Tab.Screen name="Carga" component={Carga} options={{headerShown: false}} />
-      </Tab.Navigator>
-    );
-  }
+function ProductosStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Productos"
+        component={Productos}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Camiseta"
+        component={Camiseta}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  );
+}
 
-  const Navigation = () => {
-    return (
-      <NavigationContainer>
-        <MyTabs />
-      </NavigationContainer>
-    );
-  };
+function MyTabs() {
+  return (
+    <Tab.Navigator screenOptions={{ tabBarActiveTintColor: "#FC2F00" }}>
+      <Tab.Screen
+        name="Resumen"
+        component={Home}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Camisetas"
+        component={ProductosStack}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="shirt" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Carga"
+        component={Carga}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="cloud-upload" size={size} color={color} />
+          ), 
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
 
-  export default Navigation;
+const Navigation = () => {
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
+};
+
+export default Navigation;
