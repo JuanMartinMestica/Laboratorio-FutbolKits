a373da9095a8752054bebe726df9768717903d0a
Juan Martín Mestica <juan.mestica@est.fi.uncoma.edu.ar>
2024-05-11 19:56:56 -0300

    Correción de tildes

--- Constants/Constants.js
+++ Constants/Constants.js
@@ -3,11 +3,11 @@ export const API_URL = 'https://laboratorio-apifutbol.onrender.com/api/camisetas
 export const CAMISETA_PLACEHOLDER = "https://static.vecteezy.com/system/resources/previews/010/147/858/non_2x/apparel-shirts-template-t-shirt-templates-icon-free-png.png"
 
 export const OPCIONES_CONTINENTES = [
-    { label: 'América', value: 'América' },
+    { label: 'América', value: 'America' },
     { label: 'Europa', value: 'Europa' },
     { label: 'Asia', value: 'Asia' },
-    { label: 'África', value: 'África' },
-    { label: 'Oceanía', value: 'Oceanía' }
+    { label: 'África', value: 'Africa' },
+    { label: 'Oceanía', value: 'Oceania' }
   ];
 
 
